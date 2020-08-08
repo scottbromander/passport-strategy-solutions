646d09b49586fcdb0499ff78505c5aacd275bad7
Scott Bromander <scottbromander@gmail.com>
2020-08-08 17:34:43 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,124 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sessionMiddleware = require('./modules/session-middleware');
+
+const passport = require('./strategies/user.strategy');
+const pool = require('./modules/pool');
+
+app.use(cors());
+
+// Route includes
+const userRouter = require('./routes/user.router');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ---- GOOGLE OAUTH ---- \\
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: [
+      'profile',
+      'email',
+      'https://www.googleapis.com/auth/user.birthday.read',
+    ],
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google'),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
+
+// ---- LINKEDIN OAUTH ---- \\
+
+app.get('/auth/linkedin', passport.authenticate('linkedin'), () => {});
+
+app.get(
+  '/auth/linkedin/callback',
+  passport.authenticate('linkedin', {
+    successRedirect: '/#/admin',
+    failureRedirect: '/',
+  })
+);
+
+// ---- GITHUB OAUTH ---- \\
+
+app.get(
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user:email'] })
+);
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
+// ---- SPOTIFY OAUTH ---- \\
+
+app.get(
+  '/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'],
+  }),
+  function (req, res) {}
+);
+
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
+
+// ---- FACEBOOK OAUTH ---- \\
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/#/user',
+    failureRedirect: '/',
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/#/home');
+  }
+);
+
+/* Routes */
+app.use('/api/user', userRouter);
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
---
+++ src/setupProxy.js
@@ -0,0 +1,5 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use('/auth', createProxyMiddleware({ target: 'http://localhost:5000/' }));
+};
